5ef63e47d89d5b4e04c91ad7864aad4573725800
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-24 12:30:09 +0100

    test

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,2 +1,72 @@
 /* Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine */
 
+
+//creo lista zucchine
+const obj = [
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+    {
+        varieta: 'verde',
+        peso: 40,
+        lunghezza: 20
+    },
+
+]
+
+console.log(obj);
+
+/* 
+function sumWeight(...params) {
+    let sum = 0
+    params.forEach(peso => {
+        sum += peso
+    })
+
+    return sum
+}
+
+const sumWeightZucchini = sumWeight(obj.peso)
+console.log(sumWeightZucchini); */
